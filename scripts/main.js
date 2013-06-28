68a0a9457dcd3400aa860850cdd4739fe359b9b1
Chang Liu <ch@ngliu.com>
2013-06-28 13:03:45 -0400

    updated the jQuery on the main page

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,29 @@
 $(document).ready(function() {
-  $('.dealrank').hide();
-  $('.dealrank').slideDown(1500);
+  $('.dealrank').hide().slideDown(1500);
   $('.button, .dealrank, .backMain').mouseenter(function() {
     $(this).addClass('boxshadow');
-  });
-  $('.button, .dealrank, .backMain').mouseleave(function() {
+  })
+  .mouseleave(function() {
     $(this).removeClass('boxshadow');
   });
   //  if mouse click, we show the DealRank intro div
-  $('.dealrank').click(function() {
-    $('.functionBox').toggle('slow', function() {
-      $('.dealrank').slideToggle('slow');
-      $('.dealrankIntro').slideToggle('slow');
-      $('.backMain').slideToggle('slow');
+  var dealInfo = $('.dealrankIntro');
+  var backMain = $('.backMain');
+  var functionBox = $('.functionBox');
+  var dealrank = $('.dealrank');
+  dealrank.click(function() {
+    functionBox.toggle('slow', function() {
+      dealrank.slideToggle('slow');
+      dealInfo.slideToggle('slow');
+      backMain.slideToggle('slow');
     });
   });
   // go back to the main search page
-  $('.backMain').click(function() {
-    $('.functionBox').toggle('slow');
-    $('.backMain').toggle('slow');
-    $('.dealrankIntro').toggle('slow', function() {
-       $('.dealrank').slideToggle('slow');
+  backMain.click(function() {
+    functionBox.toggle('slow');
+    $(this).toggle('slow');
+    dealInfo.toggle('slow', function() {
+      dealrank.slideToggle('slow');
     });
   });  
 });
